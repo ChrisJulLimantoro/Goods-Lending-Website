f3548935004a4e5afabd0cea73783491f459a3a8
cj <christopherlimantoro@gmail.com>
2022-11-30 19:52:09 +0700

    sementara 30/11/2022

--- homeAdmin.php
+++ homeAdmin.php
@@ -1,4 +1,5 @@
 <?php
+    include "connection.php";
     session_start();
 ?>
 <!DOCTYPE html>
--- terimaPeminjaman.php
+++ terimaPeminjaman.php
@@ -1,6 +1,11 @@
 <?php
     include "connection.php";
     session_start();
+    if(isset($_SESSION['admin']) == false){
+        session_destroy();
+        sleep(2);
+        header("Location: login.php");
+    }
 ?>
 
 <!DOCTYPE html>
