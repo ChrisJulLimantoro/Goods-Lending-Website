964a5563e82e131ba6f7e4f94c1d636cbc239aee
Nico <nicos.t.65@gmail.com>
2022-12-20 00:19:12 +0700

    sign up

--- SignUp.php
+++ SignUp.php
@@ -1,11 +1,11 @@
 <?php
     include "connection.php";
-    if(isset($_POST['user'])){
+    if(isset($_REQUEST['user'])){
         $sql = "SELECT COUNT(*) FROM `user` WHERE `username` = :username";
         $user = $_REQUEST['user'];
         $stmt = $conn->prepare($sql);
         $stmt->execute(['username' => $user]);
-        $row = $stmt->fetchColumn();
+        $row = $stmt->fetchcolumn();
         echo $row;
         exit;
     }
@@ -17,7 +17,127 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign Up</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap" rel="stylesheet">    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Secular+One&display=swap" rel="stylesheet">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Russo+One&display=swap');
+        body {
+            background-image: url("assets/background.jpg");
+            background-repeat: no-repeat;
+            background-position: center center;
+            background-attachment: fixed;
+            background-size: cover;
+            font-family: 'Russo One', sans-serif;
+            color: white !important;
+        }
+
+        #divBg {
+            background-color: hsla(0, 0%, 0%, 0.3);;
+            backdrop-filter: blur(5px);
+            box-shadow: 0px 0px 38px -15px rgba(0,0,0,0.53);
+        }
+
+        #profile {
+            border-radius:50pt; 
+            cursor:pointer;
+            transition:all 0.5s ease-in-out;
+        }
+
+        #profile:hover{
+            box-shadow: 0px 1px 25px -12px rgba(0,0,0,0.80);
+        }
+
+        #setDefault:hover{
+            transform : scale(1.15) rotate(90deg);
+            box-shadow : 0px 1px 25px -12px rgba(0,0,0,0.80);
+        }
+
+        #setDefault {
+            background-color : white;
+            border : 0px;
+            border-radius:50pt;
+            padding : 0px;
+            left: 133px;
+            bottom: -11px;
+            position: absolute;
+            transition:all 0.6s ease-in-out;
+        }
+
+        #defaultProfile{
+            background-color : white;
+            border : 0px;
+            border-radius:50%;
+            padding : 0px;
+            margin : 0px;
+            right: 45x;
+            bottom: 0px;
+            transition:all 0.6s ease-in-out;
+        }
+
+        #defaultProfile:hover{
+            transform : scale(1.15) rotate(90deg);
+            box-shadow : 0px 1px 25px -12px rgba(0,0,0,0.80);
+        }
+        .shaking{
+            animation : shake 0.3s;
+        }
+
+        .form-control{
+            border-radius: 2px;
+            background-color: white !important;
+            opacity: 0.9 !important;
+        }
+
+        @media screen and (min-width: 576px) {
+            #container {
+                margin-top: 45px;
+                margin-bottom: 45px;
+            }
+        }
+
+        @media screen and (min-width: 768px) {
+            #container {
+                margin-top: 90px;
+                margin-bottom: 90px;
+            }
+        }
+
+        @keyframes shake {
+            0% {
+                transform : translate(1px,1px) rotate(0deg); 
+            }
+            10% {
+                transform : translate(-1px,-2px) rotate(-1deg); 
+            }
+            20% {
+                transform : translate(-3px,0px) rotate(1deg); 
+            }
+            30% {
+                transform : translate(3px,2px) rotate(0deg); 
+            }
+            40% {
+                transform : translate(1px,-1px) rotate(1deg); 
+            }
+            50% {
+                transform : translate(-1px,2px) rotate(-1deg); 
+            }
+            60% {
+                transform : translate(-3px,1px) rotate(0deg); 
+            }
+            70% {
+                transform : translate(3px,1px) rotate(-1deg); 
+            }
+            80% {
+                transform : translate(-1px,-1px) rotate(1deg); 
+            }
+            90% {
+                transform : translate(1px,2px) rotate(0deg); 
+            }
+            100% {
+                transform : translate(1px,-2px) rotate(-1deg); 
+            }
+        }
+    </style>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <script>
@@ -27,6 +147,7 @@
 
                 reader.onload = function(e){
                     $("#profile").attr("src",e.target.result);
+                    console.log(e.target.result);
                 }
                 reader.readAsDataURL(input.files[0]);
             }
@@ -35,22 +156,24 @@
             let passStatus = false;
             let reStatus = false;
             let userStatus = false;
-            $("#defaultProfile").on("click",function(e){
-                e.preventDefault();
+            $('#setDefault').on('dblclick',function(){
                 $("#profile").attr("src","profile/profileDefault.jpg");
                 $("#submitFile").val("");
-            })
+                console.log("kepencet");
+            });
 
             $("#submitFile").on("change",function(){
                 read_file(this);
             })
 
             $("#profile").on("click",function(){
+                console.log($("#submitFile").val());
                 $("#submitFile").click();
             })
 
             $("#inputUsername").on("change",function(){
                 $("#inputUsername").removeClass("shaking");
+                // console.log( $("#inputUsername").val());
                 if($("#inputUsername").val().length != 0){
                 $.ajax({
                     type: "post",
@@ -166,6 +289,7 @@
                 let rePass = $("#rePass").val();
                 $("#statusRe").removeAttr("hidden");
                 $("#rePass").removeClass("shaking");
+
                 if($("#rePass").val().length == 0){
                     $("#rePassHelp").html("");
                     $("#statusRe").attr("hidden","hidden");
@@ -198,81 +322,24 @@
             });
         });
     </script>
-    <style>
-        #profile{
-            transition:all 0.5s ease-in-out;
-        }
-        #profile:hover{
-            box-shadow: 0px 1px 25px -12px rgba(0,0,0,0.80);
-        }
-        #defaultProfile{
-            background-color : white;
-            border : 0px;
-            border-radius:50%;
-            padding : 0px;
-            margin : 0px;
-            right:45px;
-            bottom:0px;
-            transition:all 0.6s ease-in-out;
-        }
-        #defaultProfile:hover{
-            transform : scale(1.15) rotate(90deg);
-            box-shadow : 0px 1px 25px -12px rgba(0,0,0,0.80);
-        }
-        .shaking{
-            animation : shake 0.3s;
-        }
-        @keyframes shake {
-            0% {
-                transform : translate(1px,1px) rotate(0deg); 
-            }
-            10% {
-                transform : translate(-1px,-2px) rotate(-1deg); 
-            }
-            20% {
-                transform : translate(-3px,0px) rotate(1deg); 
-            }
-            30% {
-                transform : translate(3px,2px) rotate(0deg); 
-            }
-            40% {
-                transform : translate(1px,-1px) rotate(1deg); 
-            }
-            50% {
-                transform : translate(-1px,2px) rotate(-1deg); 
-            }
-            60% {
-                transform : translate(-3px,1px) rotate(0deg); 
-            }
-            70% {
-                transform : translate(3px,1px) rotate(-1deg); 
-            }
-            80% {
-                transform : translate(-1px,-1px) rotate(1deg); 
-            }
-            90% {
-                transform : translate(1px,2px) rotate(0deg); 
-            }
-            100% {
-                transform : translate(1px,-2px) rotate(-1deg); 
-            }
-        }
-    </style>
 </head>
 <body>
     <div class="container position-relative">
-        <div class="row">
-            <div class="position-absolute top-0 start-50 translate-middle-x" style="width: 45em">
-                <h1 class="text-center">JUDUL</h1>
-                <h4 class="subtitle-login text-center" style="color:red; border-bottom:1px solid black;line-height:0.1em;"><span style="background-color:white; padding:0px 6px">Sign-Up</span></h4>
+        <div class="row" id="container">
+            <div class="position-absolute top-0 start-50 translate-middle-x" id="divBg" style="width: 45em;">
+                <h1 class="text-center" style="margin-top: 15px;">SIGN-UP</h1>
+                <h4 class="subtitle-login text-center" style="border-bottom:2px solid white;line-height:0.1em;"></h4>
                 <form action="insertUser.php" method="post" enctype="multipart/form-data" style="margin-top : 20px;">
                     <div class="row" style="height:auto;">
-                        <div class="col-6 col-sm-4 position-relative">
+                        <div class="col-md-6 col-sm-12 d-flex 
+                        justify-content-center position-relative">
+                            <div style="position: relative;">
                             <input type="file" class="form-control" accept=".jpg,.jpeg,.png,.svg" id="submitFile" name="submitFile" style="display:none;" value="C:\xampp\htdocs\TekWeb\Proyek\assets\profileDefault.jpg">
-                            <img id="profile" src="profile/profileDefault.jpg" width="155px" height="155px" style="border-radius:50pt; margin-left:18%;cursor:pointer;">
-                            <button class="btn btn-danger position-absolute" id="defaultProfile"><img src="assets/wrong.png" width="30px" height="30px"></button>
+                                <img id="profile" src="profile/profileDefault.jpg" width="155px" height="155px" style="border-radius: 5px;">
+                                <button class="btn btn-danger position-absolute" id="setDefault"><img src="assets/wrong.png" width="35px" height="35px"></button>
+                            </div>
                         </div>
-                        <div class="col-6 col-sm-8">
+                        <div class="col-md-6 col-sm-12">
                             <div class="row">
                                 <div class="col-12">
                                     <label for="inputFirstName" class="form-label">First Name:</label>
@@ -329,13 +396,13 @@
                     </div>
                     <div class= "row mt-4">
                         <div class="col-12">
-                            <button type="submit" class="btn btn-primary" style="width: 100%" id="signUp" disabled>Sign Up!</button>
+                            <button type="submit" class="btn btn-primary" style="width: 100%;border-radius: 2px;" id="signUp" disabled>SIGN UP</button>
                         </div>
                     </div>
                 </form>
                 <div class="row mt-4">
-                    <h4 class="text-center">Already have an account?<a href="login.php" style="text-decoration: none; color: black;"><button type="button" class="btn btn-warning mt-3" style="width: 100%;">
-                    Log in here!</button></a></h4>
+                    <h5 class="text-center">Already have an account?<a href="login.php" style="text-decoration: none; color: black;"><button type="button" class="btn btn-warning mt-3" style="width: 100%;border-radius: 2px;">
+                    LOGIN</button></a></h4>
                 </div>
             </div>
         </div>
